88032b3d1c71f32557b4226b5a8539e208a29f52
nafisanowshad <nafisanowshadmithila@gmail.com>
2026-04-16 19:33:24 +0600

    navbar section added with responsiveness

---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { RiHome2Line } from "react-icons/ri";
+import { RiTimeLine } from "react-icons/ri";
+import { PiChartLine } from "react-icons/pi";
+
+const Navbar = () => {
+    return (
+         <div className="w-11/12 mx-auto">
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <a className="text-[20px] md:text-[24px] lg:text-[27px] font-bold text-[#1F2937]">Keen<span className="text-[#244D3F]">Keeper</span></a>
+      </div>
+      <div className="navbar-end flex text-[#64748B] font-semibold">
+        <ul className="menu menu-horizontal flex-nowrap">
+          <li>
+            <a className="flex gap-0 items-center text-[15px] md:text-[17px] lg:text-[20px]"><RiHome2Line /> Home</a>
+          </li>
+          <li>
+            <a className="flex gap-0 items-center text-[15px] md:text-[17px] lg:text-[20px]"><RiTimeLine /> Timeline</a>
+          </li>
+          <li>
+            <a className="flex gap-0 items-center text-[15px] md:text-[17px] lg:text-[20px]"><PiChartLine /> Stats</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+     </div>
+    );
+};
+
+export default Navbar;
